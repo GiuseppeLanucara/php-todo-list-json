b9e04bd7abb72017bb555de1a0e599e5cdecda05
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-29 17:24:58 +0100

    spostato json e fix esercizio base

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ createApp({
     created() {
         axios.get("server.php").then((resp) => {
             this.todoList = resp.data;
+            console.log(resp.data);
         });
     }
 }).mount("#app");
